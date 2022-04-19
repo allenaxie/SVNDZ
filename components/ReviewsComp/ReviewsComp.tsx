ef5316eb150232f7ce2a66745cdc9667d023d53a
allenaxie <allenaxie1@gmail.com>
2022-04-19 14:12:01 -0700

    Create reviews component.

---
+++ components/ReviewsComp/ReviewsComp.tsx
@@ -0,0 +1,21 @@
+import classes from './ReviewsComp.module.scss';
+import { Row, Col } from 'antd';
+
+const ReviewsComp = () => {
+    return (
+        <div>
+            <Row className={classes.ratingSection}>
+                <Col></Col>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+            
+            {/* Tab Pane for Reviews */}
+            {/* Tab Pane for Questions */}
+
+            
+        </div>
+    )
+}
+
+export default ReviewsComp;
--- pages/collections/all/products/[productName]/index.tsx
+++ pages/collections/all/products/[productName]/index.tsx
@@ -20,52 +20,55 @@ const ProductDetailsPage = (props: any) => {
 
     return (
         <Row className={classes.container}>
-            <Col
-                lg={{ span: 16 }}
-                className={classes.imageGroup}
-            >
-                <Image.PreviewGroup>
-                    {images.map((image :string,index:number) => <Image width={ images.length < 3 ? 400 : 250} src={image} />)}
-                </Image.PreviewGroup>
-                {/* <img src={images[0]} /> */}
-            </Col>
-            <Col
-                lg={{ span: 8 }}
-            >
-                <Title level={2}>{name}</Title>
-                <Title level={3} className={classes.price}>${price}.00 USD</Title>
-                <Row>
-                    <Col 
-                    className={classes.sizeBtnGroup}
-                    lg={{span:8 }}
-                    >
-                        {sizes.map((size,index) => 
-                            <button 
-                                className={selectedSize === size ? `${classes.sizeBtn} ${classes.sizeActive}` : `${classes.sizeBtn}` } 
-                                onClick={(e) => handleSizeClick(e)}
-                            >
-                                {size}
-                            </button>
-                        )}
-                    </Col>
-                </Row>
-                <div className={classes.sizeGuideContainer}>
-                    <SizeGuide />
-                </div>
-                <button className={classes.addBagBtn}> ADD TO BAG </button>
-                <div className={classes.detailsContainer}>
-                    <span className={classes.description}>{description}</span>
-                    <ul className={classes.moreDetails}>
-                        {moreDetails?.map((detail:string) => 
-                            <li>
-                                <span className={classes.moreDetailsText}>
-                                    {detail}
-                                </span>
-                            </li>
-                        )}
-                    </ul>
-                </div>
-            </Col>
+            <Row className={classes.productContent}>
+                <Col
+                    lg={{ span: 16 }}
+                    className={classes.imageGroup}
+                >
+                    <Image.PreviewGroup>
+                        {images.map((image :string,index:number) => <Image width={ images.length < 3 ? 400 : 250} src={image} />)}
+                    </Image.PreviewGroup>
+                </Col>
+                <Col
+                    lg={{ span: 8 }}
+                >
+                    <Title level={2}>{name}</Title>
+                    <Title level={3} className={classes.price}>${price}.00 USD</Title>
+                    <Row>
+                        <Col 
+                        className={classes.sizeBtnGroup}
+                        lg={{span:8 }}
+                        >
+                            {sizes.map((size,index) => 
+                                <button 
+                                    className={selectedSize === size ? `${classes.sizeBtn} ${classes.sizeActive}` : `${classes.sizeBtn}` } 
+                                    onClick={(e) => handleSizeClick(e)}
+                                >
+                                    {size}
+                                </button>
+                            )}
+                        </Col>
+                    </Row>
+                    <div className={classes.sizeGuideContainer}>
+                        <SizeGuide />
+                    </div>
+                    <button className={classes.addBagBtn}> ADD TO BAG </button>
+                    <div className={classes.detailsContainer}>
+                        <span className={classes.description}>{description}</span>
+                        <ul className={classes.moreDetails}>
+                            {moreDetails?.map((detail:string) => 
+                                <li>
+                                    <span className={classes.moreDetailsText}>
+                                        {detail}
+                                    </span>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                </Col>
+            </Row>
+            
+            
         </Row>
     )
 }
