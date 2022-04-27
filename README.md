0e3c29daaf6abad3ad08f1c3edbff62c78a0e6dc
allenaxie <allenaxie1@gmail.com>
2022-04-27 12:24:21 -0700

    Fix nextjs and typescript build errors- getStaticProps & getStaticPaths

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
-<h1>SVNDZ</h1>
+# <h1 align="center"> SVNDZ </h1>
+ #### <h3 align="center">https://github.com/allenaxie/SVNDZ</h3>
+ <h5 align="center">Allen Xie</h5>
 
-Next Steps:
+<br>
+
+## 📝 Description
+
+This project is a recreation of the e-commerce store and clothing brand, SVNDZ. 
+
+## 📸 Screenshots 
+
+| | | |
+|:-------------------------:|:-------------------------:|:-------------------------:|
+|<img src="https://i.imgur.com/2EXMZ1w.png" width=200px height=110px overflow="hidden"> | <img  src="https://i.imgur.com/Fd8qgHQ.png" width=200px  height=110px overflow="hidden"> | <img src="https://i.imgur.com/jhNf6zQ.png" width=200px  height=110px overflow="hidden"> 
+|  <img src="https://i.imgur.com/8H6pEGA.png" width=200px  height=110px overflow="hidden"> | <img src="https://i.imgur.com/DSeKHPC.png" width=200px height=110px overflow="hidden"> | <img src="https://i.imgur.com/FxS3wPc.png" width=200px  height=110px overflow="hidden"> |  
+
+
+## 💻 Technologies Used 
+
+- HTML
+- CSS
+- JavaScript
+- TypeScript
+- React
+- Next.js
+- MongoDB
+- Ant Design
+- Sass
+
+## ⚛️ Getting Started
+
+<summary>How To Use</summary>
+ 
+ 
+
+Link to Deployed App -- <>
+
+## ⏭️ Next Steps
+
 - View entire look function. Add all to cart.
 - Visitors can add items to cart and checkout as guest.
--- pages/collections/all/products/[productName]/index.tsx
+++ pages/collections/all/products/[productName]/index.tsx
@@ -7,7 +7,6 @@ import { MongoClient } from 'mongodb';
 const ProductDetailsPage = ({ data }: any) => {
     const { Title } = Typography;
     // console.log('data', data);
-    const [name, price, images, description, moreDetails, sizes] = [data[0].name, data[0].price, data[0].images, data[0].description, data[0].moreDetails, data[0].sizes];
     const [selectedSize, setSelectedSize] = useState('M');
 
     const handleSizeClick = (e: any) => {
@@ -23,20 +22,20 @@ const ProductDetailsPage = ({ data }: any) => {
                     className={classes.imageGroup}
                 >
                     <Image.PreviewGroup>
-                        {data[0].images.map((image: string, index: number) => <Image alt="product photo" width={images.length < 4 ? 400 : 250} src={image} key={index} />)}
+                        {data?.images.map((image: string, index: number) => <Image alt="product photo" width={data?.images.length < 4 ? 400 : 250} src={image} key={index} />)}
                     </Image.PreviewGroup>
                 </Col>
                 <Col
                     lg={{ span: 8 }}
                 >
-                    <Title level={2}>{name}</Title>
-                    <Title level={3} className={classes.price}>${price}.00 USD</Title>
+                    <Title level={2}>{data?.name}</Title>
+                    <Title level={3} className={classes.price}>${data?.price}.00 USD</Title>
                     <Row>
                         <Col
                             className={classes.sizeBtnGroup}
                             lg={{ span: 8 }}
                         >
-                            {sizes.map((size: any, index: number) =>
+                            {data?.sizes.map((size: any, index: number) =>
                                 <button
                                     className={selectedSize === size ? `${classes.sizeBtn} ${classes.sizeActive}` : `${classes.sizeBtn}`}
                                     onClick={(e) => handleSizeClick(e)}
@@ -52,9 +51,9 @@ const ProductDetailsPage = ({ data }: any) => {
                     </div>
                     <button className={classes.addBagBtn}> ADD TO BAG </button>
                     <div className={classes.detailsContainer}>
-                        <span className={classes.description}>{description}</span>
+                        <span className={classes.description}>{data?.description}</span>
                         <ul className={classes.moreDetails}>
-                            {moreDetails?.map((detail: string, index: number) =>
+                            {data?.moreDetails.map((detail: string, index: number) =>
                                 <li key={index}>
                                     <span className={classes.moreDetailsText}>
                                         {detail}
@@ -71,42 +70,29 @@ const ProductDetailsPage = ({ data }: any) => {
 
 export default ProductDetailsPage;
 
-// export async function getStaticPaths() {
+export async function getStaticPaths() {
 
-//     const res = await fetch('http://localhost:3000/api/fetchProducts')
-//     const data = await res.json();
+    const client = await MongoClient.connect(`mongodb+srv://${process.env.NEXT_PUBLIC_MONGODB_USER}:${process.env.NEXT_PUBLIC_MONGODB_PASS}@svndz.okep3.mongodb.net/Products?retryWrites=true&w=majority`);
+    const db = client.db();
+    const productsCollection = db.collection('Collections');
+  
+    const data = await productsCollection.find().toArray();
+    // client.close();
+    // console.log('dataaaaa',data)
+    const paths = data.map((product: any) => {
+        return {
+            params: {
+                productName: `${product?.name}`,
+            }
+        }
+    })
+    return {
+        fallback: false,
+        paths: paths,
+    }
+}
 
-//     const paths = data.map((product: any) => {
-//         return {
-//             params: {
-//                 productName: `${product.name}`
-//             }
-//         }
-//     })
-//     return {
-//         fallback: true,
-//         paths: paths,
-//     }
-// }
-
-// export async function getStaticProps(context: any) {
-
-//     const client = await MongoClient.connect(`mongodb+srv://${process.env.NEXT_PUBLIC_MONGODB_USER}:${process.env.NEXT_PUBLIC_MONGODB_PASS}@svndz.okep3.mongodb.net/Products?retryWrites=true&w=majority`);
-//     const db = client.db();
-//     const productsCollection = db.collection('Collections');
-//     // console.log('context', context.params.productName);
-//     const res = await productsCollection.find().toArray();
-//     //   console.log('res', res);
-//     const data = JSON.parse(JSON.stringify(res.filter(product => product.name === context.params.productName)));
-//     client.close();
-//     return {
-//         props: {
-//             data: data
-//         }
-//     }
-// }
-
-export async function getServerSideProps(context:any) {
+export async function getStaticProps(context: any) {
 
     const client = await MongoClient.connect(`mongodb+srv://${process.env.NEXT_PUBLIC_MONGODB_USER}:${process.env.NEXT_PUBLIC_MONGODB_PASS}@svndz.okep3.mongodb.net/Products?retryWrites=true&w=majority`);
     const db = client.db();
@@ -114,12 +100,14 @@ export async function getServerSideProps(context:any) {
     // console.log('context', context.params.productName);
     const res = await productsCollection.find().toArray();
     //   console.log('res', res);
-    const data = JSON.parse(JSON.stringify(res.filter(product => product.name === context.params.productName)));
+    const data = JSON.parse(JSON.stringify(res.filter(product => product?.name === context.params.productName)));
     // client.close();
     return {
         props: {
-            data: data
+            data: data[0]
         }
     }
-  }
+}
+
+
 
